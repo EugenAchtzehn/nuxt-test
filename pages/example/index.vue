ec0fdc3718590f386d77d1046437f90490570b62
remove <b508099021@tmu.edu.tw>
2022-09-28 09:59:59 +0800

    [Add] some testing results about TailwindCSS

--- pages/example/index.vue
+++ pages/example/index.vue
@@ -2,5 +2,11 @@
   <div>
     <h2>index.vue</h2>
     <p>path "/example" 會進入這頁</p>
+    <p class="bg-stone-300 text-center">
+      Not sure why, but it seems some color settings of TailwindCSS are not
+      working on this project, including background-color and border-color.
+      <br />
+      However, the text-color works.
+    </p>
   </div>
 </template>
