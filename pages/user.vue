d932929dd3b8034b70412395c280b2b534312fcb
EugenAchtzehn <eugene1826@gmail.com>
2022-10-02 18:38:21 +0800

    [Add] the official doc of NuxtTailwind is important.

--- pages/user.vue
+++ pages/user.vue
@@ -1,10 +1,28 @@
 <template>
-  <h1 class="text-2xl text-center font-bold">
-    This is the user page.
-  </h1>
+  <div>
+    <h1 class="text-2xl text-center font-bold">This is the user page.</h1>
+    <div class="badge">Testing</div>
+    <p>
+      <a
+        target="_blank"
+        href="https://tailwindcss.nuxtjs.org/getting-started/setup"
+      >
+        After taking official doc as reference. Somehow the color settings work.
+      </a>
+    </p>
+  </div>
 </template>
 <script>
 export default {
-  name: 'UserPage'
-}
+  name: "UserPage",
+};
 </script>
+
+<style lang="postcss" scoped>
+.badge {
+  @apply bg-gray-200 rounded-full inline-block font-semibold;
+  &:hover {
+    @apply bg-gray-300;
+  }
+}
+</style>
